d2a63511d20301d125fa1d994aa0d1e8edb760b7
Daniel <kaziev30@mail.ru>
2024-06-22 01:25:59 +0500

    draft: image visibility

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: true,
-  assetPrefix: process.env.NODE_ENV === "production" ? "./" : "", 
+  assetPrefix: process.env.NODE_ENV === "production" ? "/kanban-frontend/" : "",
 };
 
 export default nextConfig;
