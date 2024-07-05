72cd2ecd211a484b5d10f094adf56fb13497f915
Daniel <kaziev30@mail.ru>
2024-07-05 16:43:19 +0500

    fix: dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
-export default function Home() {
-  console.log(process.env.BACKEND_URL);
-  
-  return <>Hello world</>
+"use client"
+import { Paper } from "@mui/material";
+
+export default function Home({ children }: { children: React.ReactNode }) {
+  return <>
+  <Paper>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptatum aperiam officia, necessitatibus distinctio quisquam aliquam rem pariatur et accusantium sunt quae minima autem animi nulla numquam saepe ex accusamus.</Paper>
+  </>
 }
