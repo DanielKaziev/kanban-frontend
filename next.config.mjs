074f671297edc26ce1a11f9d4c027e6e33c1f296
Daniel Kaziev <43777012+DanielKaziev@users.noreply.github.com>
2024-07-12 23:18:49 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  assetPrefix: process.env.NODE_ENV === "production" ? "/kanban-frontend/" : "",
 };
 
 export default nextConfig;
